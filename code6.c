f43490b74f4cdb905deacf521bdc5c1da1b72399
ShakshamKumar10 <shaksham705@gmail.com>
2026-09-11 20:33:57 +0530

    Create code6.c

---
+++ code6.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a[100], key;
+    int low, high, mid, i;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(a[mid] == key)
+        {
+            printf("Found at index %d", mid);
+            return 0;
+        }
+        else if(a[mid] < key)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    printf("-1");
+
+    return 0;
+}
