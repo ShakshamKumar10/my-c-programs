f190f1d5b89a1fbf6c9ec2caa29faf4339719924
ShakshamKumar10 <shaksham705@gmail.com>
2026-09-17 19:03:04 +0530

    Add matrix diagonal sum calculation in code19.c

---
+++ code19.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c, i, j, sum = 0;
+
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        sum = sum + a[i][i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
