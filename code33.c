1f9941009bf1baa551e7a83710d5d93b0c803e7d
ShakshamKumar10 <shaksham705@gmail.com>
2026-09-24 22:03:10 +0530

    Add program to find first repeating alphabet

---
+++ code33.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count[26] = {0};
+    int i;
+
+    scanf("%s", str);
+
+    // Count frequency of each alphabet
+    for (i = 0; str[i] != '\0'; i++) {
+        count[str[i] - 'a']++;
+    }
+
+    // Find the first repeating alphabet
+    for (i = 0; str[i] != '\0'; i++) {
+        if (count[str[i] - 'a'] > 1) {
+            printf("%c", str[i]);
+            return 0;
+        }
+    }
+
+    printf("No repeating alphabet");
+
+    return 0;
+}
