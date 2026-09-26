4f1e7e8fd3148863734688e20689513635558099
ShakshamKumar10 <shaksham705@gmail.com>
2026-09-26 23:31:28 +0530

    Create code37.c

---
+++ code37.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[200];
+    int i, start = 0, end, j;
+    char temp;
+
+    fgets(str, sizeof(str), stdin);
+
+    int len = strlen(str);
+
+    for (i = 0; i <= len; i++)
+    {
+        if (str[i] == ' ' || str[i] == '\n' || str[i] == '\0')
+        {
+            end = i - 1;
+
+            for (j = start; j < end; j++, end--)
+            {
+                temp = str[j];
+                str[j] = str[end];
+                str[end] = temp;
+            }
+
+            start = i + 1;
+        }
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
